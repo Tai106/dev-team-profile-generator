ebd151d793a33771c7e6211f9c7b713cb006e831
Gravity <tevoned@gmail.com>
2022-02-26 18:00:51 -0500

    initial files

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,20 @@
+const Employee = require('../lib/Employee');
+const employee = new Employee('tai', '1', 'taivonedwards1@gmail.com');
+
+test('test to produce constructor values for the employee object', () => {
+    expect(employee.name).toBe('tai');
+    expect(employee.id).toBe('1');
+    expect(employee.email).toBe('taivonedwards1@gmail.com');
+});
+
+test('test to get the name from the getNmae() method', () => {
+    expect(employee.getName()).toBe('tai');
+});
+
+test('test to get the id from the getId() method', () => {
+    expect(employee.getId()).toBe('1');
+});
+
+test('test to get the email from the getEmail() method', () => {
+    expect(employee.getEmail()).toBe('taivonedwards1@gmail.com');
+})
