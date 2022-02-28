a6b6347fa640d97e4cbde1258514ea3045a5d08c
Gravity <tevoned@gmail.com>
2022-02-27 19:45:31 -0500

    updated files

--- index.js
+++ index.js
@@ -18,33 +18,118 @@ const promptManager = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the Managers name? (Required)',
+            message: 'What is your name?',
         },
         {
             type: 'input',
-            name: 'id',
-            message: 'What is the Mangers ID?',  
+            name: 'employeeId',
+            message: 'Enter your employee ID',
         },
         {
             type: 'input',
             name: 'email',
-            message: 'What is the Managers email address?',
+            message: 'Enter your email address',
         },
         {
             type: 'input',
             name: 'officeNumber',
-            message: 'What is the managers office number?',
+            message: 'Enter your office number',
         },
-    ]);
+    ]).then(answers => {
+        console.log(answers);
+        const manager = new Manager(answers.name, answers.employeeId, answers.email, answers.officeNumber);
+        teamMembers.push(manager);
+        promptMenu();
+    })
+};
 
-    const promptEngineer = () => {
+const promptMenu = () => {
+return inquirer.prompt([
+        {
+            type: 'list',
+            name: 'menu',
+            message: 'Please select the option you would like to continue with:',
+            choices: ['add an engineer', 'add an intern', 'finish building my team']
+        }])
+        .then(userChoice => {
+            switch (userChoice.menu) {
+                case "add an engineer":
+                    promptEngineer();
+                    break;
+                case "add an intern":
+                    promptIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+};
+
+const promptEngineer = () => {
+return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of engineer?',
+        },
+        {
+            type: 'input',
+            name: 'employeeId',
+            message: 'Enter your employee ID',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Enter your Github username.',
+        }
+        ]).then(answers => {
+        console.log(answers);
+        const engineer = new Engineer(answers.name, answers.employeeId, answers.email, answers.githubUsername);
+        teamMembers.push(engineer);
+        promptMenu();
+        })
+        };
+
+        const promptIntern = () => {
         return inquirer.prompt([
-            {
+        {
             type: 'input',
             name: 'name',
-            message: 'What is the name of the engineer? (Required)',
-            },
-            {
+            message: 'What is the name of the intern?',
+        },
+        {
             type: 'input',
-            name: 'name',
-            message: 'What is the name of the engineer? (Required)',
+            name: 'employeeId',
+            message: 'Enter your employee ID',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Enter your school name.',
+        }
+    ]).then(answers => {
+        console.log(answers);
+        const intern = new Intern(answers.name, answers.employeeId, answers.email, answers.school);
+        teamMembers.push(intern);
+        promptMenu();
+    })
+    };
+
+    const buildTeam = () => {
+   
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, generateSite(teamMembers), "utf-8");
+    }
+    promptManager();
